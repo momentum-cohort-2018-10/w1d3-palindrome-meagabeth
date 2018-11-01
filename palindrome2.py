3ca258746d59452c648b7a3421dbecb93ad91bde
Mo Glatz <meagabeth@icloud.com>
2018-11-01 09:50:30 -0400

    Revised as of Thurs morning, still incomplete

--- palindrome2.py
+++ palindrome2.py
@@ -1,6 +1,6 @@
 text = input('Type a sentence to verify if it is a palindrome: ')
 
-def clean_text(text):
+def clean_text(text_we_want_to_clean):
     """ Given text, return the text with no spaces or punctuation and all lowercased."""
 
     # new_text = ""
@@ -13,18 +13,30 @@ def clean_text(text):
     # print(new_text)
 
     new_text = ""
-    text = text.lower()
-    text = text.replace(" ", "")
-    for char in text:
+    text_we_want_to_clean = text_we_want_to_clean.lower()
+    text_we_want_to_clean = text_we_want_to_clean.replace(" ", "")
+    for char in text_we_want_to_clean:
         if char.isalpha():
             new_text = new_text + char
+    print(new_text)
     return new_text
+
+def no_numbers(words_only):
+    for character in words_only:
+        if character.isdigit():
+            return False
+
     
 
 def inner(new_text):
     """
     Do the first and last letters match? If yes, reassess the inner letters; do the first and last match? repeat.
     """
+    if no_numbers(text) == False:
+        print("Invalid Input.")
+    
+
+    new_text = clean_text(new_text)
     print(new_text)
     if len(new_text) == 0:
         return True
